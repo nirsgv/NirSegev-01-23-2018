27e00f007d70560563cb71242fead04b710007e6
nirsgv <nirsegevmail@gmail.com>
2020-06-27 13:43:40 +0300

    darl mode color scheme implemented

--- src/components/checkbox.js
+++ src/components/checkbox.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SvgSprite from "./svgSprite";
-import {imgData} from "../data/localImgData";
+import { imgData } from "../data/localImgData";
 import PropTypes from 'prop-types';
 
 function Checkbox({ ...restProps }) {
@@ -10,16 +10,17 @@ function Checkbox({ ...restProps }) {
     return (
         <div className={"checkbox__wrap"}  >
             <label htmlFor={checkboxId}>
+
                 <span>
                     {labelText}
                 </span>
-
                 <input type="checkbox"
                        id={checkboxId}
                        name={name}
                        checked={checked}
                        onChange={onChangeCb}
                 />
+
                 <SvgSprite classes={'icon-logo'} src={imgData.sprite.src} alt={imgData.sprite.description} name={'CHECK'} />
             </label>
 
